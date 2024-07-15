eb1db4f1ff0ab97b651e59d2918915ec4060eee7
Velin Kamenov <velinkamenovtues@gmail.com>
2024-07-15 15:37:33 +0300

    to check

---
+++ task6-2.js
@@ -0,0 +1,17 @@
+function pluvane(input){
+let worldRecord = Number(input[0]);
+let distInMetres = Number(input[1]);
+let secPerMetres = Number(input[2]);
+let distance = distInMetres * secPerMetres;
+let abscence = (distInMetres/15) * 12.5;
+abscence = Math.floor(abscence);
+let run = distance + abscence;
+let a = run - worldRecord;
+if(run > worldRecord){
+    console.log(`No, he failed! He was ${a.toFixed(2)} seconds slower.`);
+}
+else {
+    console.log(`Yes, he succeeded! The new world is ${run.toFixed(2)} seconds.`);
+}
+}
+pluvane(["55555.67","3017","5.03"]);
